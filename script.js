9885aebd14d91f94d2717c98bb94c7cbde02a6f7
Uday Pratap <udaypratap169@gmail.com>
2020-09-29 22:58:05 +0530

    updating

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 new Vue ({
 	el: '#app',
 	data: {
-		title: 'Hello World'
-	},
-	methods:{
-		changeTitle: function (event) {
-			this.title = event.target.value;
-		}
+		products: 'Boot',
+		description: 'Hi,this is description'
 	}
 })
